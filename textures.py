f4ab9dcee8c7e3549ac8057e1edc5517a54a1ba0
jinj <jinj@mit.edu>
2023-12-11 23:40:01 -0500

    nothing significant

--- textures.py
+++ textures.py
@@ -7,7 +7,7 @@ class Textures:
         self.ctx = app.ctx
 
         # Load texture
-        self.texture_0 = self.load('stone.png')
+        self.texture_0 = self.load('grass.png')
 
         # Assign texture unit (0)
         self.texture_0.use(location = 0)
